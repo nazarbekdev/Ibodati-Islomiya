f03a43dc03f2a0bea8c875dc2066048e8f7c83a5
Nazarbek-Kabulov <nqobulov571@gmail.com>
2024-04-16 17:54:53 +0500

    upadate admin

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from .models import Mavzu
+from .models import Mavzu, UsersBot, Qism, Bolim
 # Register your models here.
-admin.site.register((Mavzu,))
+
+
+class MavzuAdmin(admin.ModelAdmin):
+    list_display = ('savol', 'javob', 'created_at', 'qism', 'bolim')
+    search_fields = ('savol',)
+
+
+class UsersBotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user_id', 'created')
+    search_fields = ('name', 'user_id')
+
+
+class QismAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class BolimAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Mavzu, MavzuAdmin)
+admin.site.register(UsersBot, UsersBotAdmin)
+admin.site.register(Qism, QismAdmin)
+admin.site.register(Bolim, BolimAdmin)
